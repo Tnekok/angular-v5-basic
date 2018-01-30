369f6e2a614c9af2038b5e2697bfa9e459105599
JesusMG <Jnekoksop@gmail.com>
2018-01-30 14:30:04 +0100

    shopping-list service functionality and cross component update on changes

--- src/app/shopping-list/shared/services/shopping-list.service.ts
+++ src/app/shopping-list/shared/services/shopping-list.service.ts
@@ -1,3 +1,21 @@
+import { EventEmitter } from '@angular/core';
+
+import { Ingredient } from '../../../shared/models/ingredient.model';
+
 export class ShoppingListService {
 
+    public ingredientsChanged = new EventEmitter<Ingredient[]>();
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apple', 5),
+        new Ingredient('Tomato', 10)
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
