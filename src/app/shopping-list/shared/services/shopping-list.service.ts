55452dd1a56f28bda4f3e65abfe608b64eefc0eb
JesusMG <Jnekoksop@gmail.com>
2018-01-31 15:23:11 +0100

    reactive form in shopping-list

--- src/app/shopping-list/shared/services/shopping-list.service.ts
+++ src/app/shopping-list/shared/services/shopping-list.service.ts
@@ -5,6 +5,8 @@ import { Ingredient } from '../../../shared/models/ingredient.model';
 export class ShoppingListService {
 
     public ingredientsChanged = new Subject<Ingredient[]>();
+    public startedEditing = new Subject<number>();
+
     private ingredients: Ingredient[] = [
         new Ingredient('Apple', 5),
         new Ingredient('Tomato', 10)
@@ -22,6 +24,21 @@ export class ShoppingListService {
     addIngredients(ingredients: Ingredient[]) {
         // " ... " <-- Spread operator (ES6): Turns an array of elements into a list of elements
         this.ingredients.push(...ingredients);
+        this.emitChanges();
+    }
+
+    getIngredient(index: number) {
+        return this.ingredients[index];
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.emitChanges();
+    }
+
+    deleteIngredient(index: number) {
+        // splice(index, n) allows us to start at an index position in the array and remove n elements
+        this.ingredients.splice(index, 1);
         this.emitChanges();
     }
 
