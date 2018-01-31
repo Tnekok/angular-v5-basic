6e2802fe2abcc23d50e3b206e5b020b818d6727a
JesusMG <Jnekoksop@gmail.com>
2018-01-31 11:52:36 +0100

    routing, route params and dynamic route params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBaseComponent } from './recipes/recipe-base/recipe-base.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes', component: RecipesComponent },
+    { path: 'recipes', component: RecipesComponent, children: [
+        { path: '', component: RecipeBaseComponent },
+        { path: ':id', component: RecipeDetailComponent },
+    ] },
     { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { RecipesService } from '../shared/services/recipes.service';
 import { Recipe } from '../shared/models/recipe.model';
@@ -10,11 +11,21 @@ import { Recipe } from '../shared/models/recipe.model';
 })
 export class RecipeDetailComponent implements OnInit {
 
-  @Input() recipe: Recipe;
+  public recipe: Recipe;
+  public id: number;
 
-  constructor(private recipesService: RecipesService) { }
+  constructor(
+    private recipesService: RecipesService,
+    private route: ActivatedRoute) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.recipe = this.recipesService.getRecipe(this.id);
+      }
+    );
+  }
 
   onAddToShoppingList() {
     this.recipesService.addIngredientsToShoppingList(this.recipe.ingredients);
