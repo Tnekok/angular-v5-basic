743441b8f729adfe7d9066fc0e544fbdc7b0f06c
JesusMG <Jnekoksop@gmail.com>
2018-02-01 15:41:02 +0100

    shopping-list and auth modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 
 import { AuthGuard } from './auth/shared/services/auth-guard.service';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'shopping-list', component: ShoppingListComponent },
-    { path: 'signup', component: SignupComponent },
-    { path: 'signin', component: SigninComponent }
+    { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
 @NgModule({
