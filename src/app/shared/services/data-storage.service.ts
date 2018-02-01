c3a787b4a89e7c4cb674c3037ad5904d28f39b12
JesusMG <Jnekoksop@gmail.com>
2018-02-01 17:41:17 +0100

    implemented params as a part of the new structure instead of a part of the url

--- src/app/shared/services/data-storage.service.ts
+++ src/app/shared/services/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
 import { RecipesService } from '../../recipes/shared/services/recipes.service';
@@ -22,19 +22,30 @@ export class DataStorageService {
         const token = this.authService.getToken();
         let recipes = this.recipesService.getRecipes();
         recipes = this.formatRecipes(recipes);
-        return this.httpClient.put(this.firebaseUrl + 'recipes.json?auth=' + token, recipes);
+        return this.httpClient.put(this.firebaseUrl + 'recipes.json', recipes, {
+            observe: 'body',
+            responseType: 'json',
+            params: new HttpParams().set('auth', token)  // <-- works the same as HttpHeaders as functions is related
+        });
     }
 
     getRecipes() {
         const token = this.authService.getToken();
-        this.httpClient.get<Recipe[]>(this.firebaseUrl + 'recipes.json?auth=' + token)
-            .map((recipes: Recipe[]) => {
-                recipes = this.formatRecipes(recipes);
-                return recipes;
-            })
-            .subscribe(
-                (recipes: Recipe[]) => this.recipesService.setRecipes(recipes)
-            );
+        this.httpClient.get<Recipe[]>(this.firebaseUrl + 'recipes.json', {
+            // these headers wont work in out project due to firebase expecting an auth as params
+            // headers: new HttpHeaders().set('Authorization', 'Bearer dsfasfasfsafsadfas'),  <-- for 1 .set()
+            // headers: new HttpHeaders().set('Authorization', 'Bearer dsfasfasfsafsadfas').append(), <-- for many set().append().append()
+            observe: 'body', // response (for full response data), body (only the body of the response), ...
+            responseType: 'json', // json, text, blob, arraybuffer, ...
+            params: new HttpParams().set('auth', token)  // <-- works the same as HttpHeaders as functions is related
+        })
+        .map((recipes: Recipe[]) => {
+            recipes = this.formatRecipes(recipes);
+            return recipes;
+        })
+        .subscribe(
+            (recipes: Recipe[]) => this.recipesService.setRecipes(recipes)
+        );
     }
 
     private formatRecipes(recipes) {
