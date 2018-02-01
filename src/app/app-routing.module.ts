2f6e1cf2718783ce05c49b3504efa597e0d116e9
JesusMG <Jnekoksop@gmail.com>
2018-02-01 12:07:01 +0100

    implementation of signin logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { RecipeBaseComponent } from './recipes/recipe-base/recipe-base.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -17,7 +18,8 @@ const appRoutes: Routes = [
         { path: ':id/edit', component: RecipeEditComponent }
     ] },
     { path: 'shopping-list', component: ShoppingListComponent },
-    { path: 'signup', component: SignupComponent }
+    { path: 'signup', component: SignupComponent },
+    { path: 'signin', component: SigninComponent }
 ];
 
 @NgModule({
--- src/app/auth/shared/services/auth.service.ts
+++ src/app/auth/shared/services/auth.service.ts
@@ -6,4 +6,10 @@ export class AuthService {
         firebase.auth().createUserWithEmailAndPassword(email, password)
             .catch(error => console.log('signupUser error: ', error));
     }
+
+    signinUser(email: string, password: string) {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(response => console.log('login success: ', response))
+            .catch(error => console.log('login error: ', error));
+    }
 }
